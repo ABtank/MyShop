4ffb976f67df315f4a62f185c4f6e293ad3046ae
Iurii <abtank@outlook.com>
2020-10-26 11:24:16 +0300

    Initial commit

---
+++ shop-for-admin-ui/src/main/java/ru/abtank/spring_shop/shopforadminui/ShopForAdminUiApplication.java
@@ -0,0 +1,13 @@
+package ru.abtank.spring_shop.shopforadminui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopForAdminUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopForAdminUiApplication.class, args);
+	}
+
+}
---
+++ shop-for-admin-ui/src/test/java/ru/abtank/spring_shop/shopforadminui/ShopForAdminUiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.abtank.spring_shop.shopforadminui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopForAdminUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
